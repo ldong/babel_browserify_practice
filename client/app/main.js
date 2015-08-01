3224e634742d7c66ebfa5f2f24044a5469d19854
Lin Dong <ldong@Lins-MacBook-Pro.local>
2015-08-01 11:43:53 -0700

    Added ES6 generators examples

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,14 @@
 'use strict';
 
+require('babel/polyfill');
 var utils = require('./utils');
 var iso = require('../../portable');
+var fib = iso.fib;
 
 console.log('Client side code started');
 console.log(iso.validateId('USER_1231241'));
 
-utils.count();
+// utils.count();
+console.log(fib.next());
+console.log(fib.next());
+console.log(fib.next());
--- portable/index.js
+++ portable/index.js
@@ -1,11 +1,28 @@
 'use strict';
 
 module.exports = {
-  validateId
+  validateId,
+  fib: fib()
 };
 
 const ID_PREFIX = 'USER_';
 
 function validateId(id) {
   return typeof id == 'string' && id.indexOf(ID_PREFIX) === 0;
+}
+
+function* fib() {
+  var n1 = 1;
+  var n2 = 1;
+
+  while(true){
+    var current = n2;
+    n2 = n1;
+    n1 += current;
+    var reset = yield current;
+    if (reset){
+      n1 = 1;
+      n2 = 1;
+    }
+  }
 }
